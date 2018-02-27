083125177a7e604418cf506b6c978f454dc3d65e
Juan Moreno <=>
2018-02-26 20:15:55 -0500

    se implemento population

---
+++ src/Model/Impl/ColorPopulation.java
@@ -0,0 +1,26 @@
+package Model.Impl;
+
+import Model.Abstraction.*;
+
+public class ColorPopulation implements Population{
+
+	private ColorAgent [][] agents;
+	public ColorPopulation() {
+		agents = new ColorAgent[Core.getMaxX()][Core.getMaxY()];
+		for(int i =0 ; i< Core.getMaxX();i++) {
+			for(int j=0; i< Core.getMaxY();j++){
+				agents[i][j]= new ColorAgent();
+			}
+		}
+	}
+	
+	@Override
+	public void update() {
+		for(int i =0 ; i< Core.getMaxX();i++) {
+			for(int j=0; i< Core.getMaxY();j++){
+				agents[i][j].iterate();
+			}
+		}
+	}
+
+}
